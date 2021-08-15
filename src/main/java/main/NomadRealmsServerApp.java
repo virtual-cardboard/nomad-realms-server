07465e40c5fa7c4691d1d781bcb920bace38596d
Lunkle <bobbob.donny@gmail.com>
2021-08-15 13:42:59 -0400

    Bootstrap Server class

---
+++ src/main/java/main/NomadRealmsServerApp.java
@@ -0,0 +1,11 @@
+package main;
+
+import bootstrap.NomadRealmsBootstrapServer;
+
+public class NomadRealmsServerApp {
+
+	public static void main(String[] args) {
+		new NomadRealmsBootstrapServer().start();
+	}
+
+}
