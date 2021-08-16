beddff092a6d4353ae613c628c12377f727e9490
Lunkle <bobbob.donny@gmail.com>
2021-08-16 11:47:42 -0400

    Hole Punch Test

--- src/main/java/main/NomadRealmsServerApp.java
+++ src/main/java/main/NomadRealmsServerApp.java
@@ -1,11 +1,18 @@
 package main;
 
+import java.net.SocketException;
+
 import bootstrap.NomadRealmsBootstrapServer;
 
 public class NomadRealmsServerApp {
 
 	public static void main(String[] args) {
-		new NomadRealmsBootstrapServer().start();
+		try {
+			NomadRealmsBootstrapServer bootstrap = new NomadRealmsBootstrapServer();
+			bootstrap.start();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
