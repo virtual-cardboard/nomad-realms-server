79a78d819f86074ae376073ba3336201f2044acb
Lunkle <bobbob.donny@gmail.com>
2021-08-17 10:14:10 -0400

    STUN Server

--- src/main/java/main/NomadRealmsServerApp.java
+++ src/main/java/main/NomadRealmsServerApp.java
@@ -2,14 +2,16 @@ package main;
 
 import java.net.SocketException;
 
-import bootstrap.NomadRealmsBootstrapServer;
+import bootstrap.STUNServer;
 
 public class NomadRealmsServerApp {
 
 	public static void main(String[] args) {
 		try {
-			NomadRealmsBootstrapServer bootstrap = new NomadRealmsBootstrapServer();
-			bootstrap.start();
+//			NomadRealmsBootstrapServer bootstrap = new NomadRealmsBootstrapServer();
+//			bootstrap.start();
+			STUNServer stun = new STUNServer();
+			stun.start();
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
