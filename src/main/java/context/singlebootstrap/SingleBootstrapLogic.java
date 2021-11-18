4311314431c1ee13260b846b00fa3b8672d1be2e
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-11-18 17:16:17 -0500

    Use handlers instead of instanceof checks

--- src/main/java/context/singlebootstrap/SingleBootstrapLogic.java
+++ src/main/java/context/singlebootstrap/SingleBootstrapLogic.java
@@ -15,43 +15,44 @@ public class SingleBootstrapLogic extends GameLogic {
 	private long nonce = new Random().nextLong();
 
 	@Override
-	public void update() {
-		SingleBootstrapData data = (SingleBootstrapData) context().data();
-		while (!eventQueue().isEmpty()) {
-			GameEvent event = eventQueue().poll();
-			if (event instanceof BootstrapRequestEvent) {
-				BootstrapRequestEvent bootstrapRequestEvent = (BootstrapRequestEvent) event;
-				System.out.println("Received bootstrap request");
-				if (!data.gotFirstPacket) {
-					data.lan = bootstrapRequestEvent.getLan();
-					data.wan = bootstrapRequestEvent.source().getAddress();
-					data.gotFirstPacket = true;
-				} else {
-					System.out.println("Giving out nonce: " + nonce);
-					PacketModel packet1 = BOOTSTRAP_RESPONSE.builder(data.wan)
-							.consume(currentTimeMillis())
-							.consume(nonce)
-							.consume(bootstrapRequestEvent.getLan().ip())
-							.consume((short) bootstrapRequestEvent.getLan().port())
-							.consume(bootstrapRequestEvent.source().getAddress().ip())
-							.consume((short) bootstrapRequestEvent.source().getAddress().port())
-							.build();
-					context().sendPacket(packet1);
-					PacketModel packet2 = BOOTSTRAP_RESPONSE.builder(bootstrapRequestEvent.source().getAddress())
-							.consume(currentTimeMillis())
-							.consume(nonce)
-							.consume(data.lan.ip())
-							.consume((short) data.lan.port())
-							.consume(data.wan.ip())
-							.consume((short) data.wan.port())
-							.build();
-					context().sendPacket(packet2);
-					System.out.println("Done sending bootstrap response packet");
-				}
+	protected void init() {
+		addHandler(BootstrapRequestEvent.class, event -> true, bootstrapRequestEvent -> {
+			SingleBootstrapData data = (SingleBootstrapData) context().data();
+			System.out.println("Received bootstrap request");
+			if (!data.gotFirstPacket) {
+				data.lan = bootstrapRequestEvent.getLan();
+				data.wan = bootstrapRequestEvent.source().getAddress();
+				data.gotFirstPacket = true;
 			} else {
-				throw new RuntimeException("Was expecting Bootstrap Request");
+				System.out.println("Giving out nonce: " + nonce);
+				PacketModel packet1 = BOOTSTRAP_RESPONSE.builder(data.wan)
+						.consume(currentTimeMillis())
+						.consume(nonce)
+						.consume(bootstrapRequestEvent.getLan().ip())
+						.consume((short) bootstrapRequestEvent.getLan().port())
+						.consume(bootstrapRequestEvent.source().getAddress().ip())
+						.consume((short) bootstrapRequestEvent.source().getAddress().port())
+						.build();
+				context().sendPacket(packet1);
+				PacketModel packet2 = BOOTSTRAP_RESPONSE.builder(bootstrapRequestEvent.source().getAddress())
+						.consume(currentTimeMillis())
+						.consume(nonce)
+						.consume(data.lan.ip())
+						.consume((short) data.lan.port())
+						.consume(data.wan.ip())
+						.consume((short) data.wan.port())
+						.build();
+				context().sendPacket(packet2);
+				System.out.println("Done sending bootstrap response packet");
 			}
-		}
+		}, true);
+		addHandler(GameEvent.class, event -> {
+			throw new RuntimeException("Was expecting Bootstrap Request");
+		});
+	}
+
+	@Override
+	public void update() {
 	}
 
 }
--- src/main/java/context/stun/STUNLogic.java
+++ src/main/java/context/stun/STUNLogic.java
@@ -11,26 +11,26 @@ import event.STUNRequestEvent;
 public class STUNLogic extends GameLogic {
 
 	@Override
-	public void update() {
-		while (!eventQueue().isEmpty()) {
-			GameEvent event = eventQueue().poll();
-			if (event instanceof STUNRequestEvent) {
-				STUNRequestEvent stunResponseEvent = (STUNRequestEvent) event;
-				System.out.println("Received STUN request from " + stunResponseEvent.source().description());
-				PacketAddress address = stunResponseEvent.source().getAddress();
-				PacketModel packet = STUN_RESPONSE.builder(address)
-						.consume(System.currentTimeMillis())
-						.consume(stunResponseEvent.getNonce())
-						.consume(address.ip())
-						.consume(address.shortPort())
-						.build();
-				System.out.println("Nonce: " + stunResponseEvent.getNonce());
-				context().sendPacket(packet);
-			} else {
-				System.out.println("Unhandled event type: " + event.getClass());
-			}
-		}
+	protected void init() {
+		addHandler(STUNRequestEvent.class, event -> true, stunResponseEvent -> {
+			System.out.println("Received STUN request from " + stunResponseEvent.source().description());
+			PacketAddress address = stunResponseEvent.source().getAddress();
+			PacketModel packet = STUN_RESPONSE.builder(address)
+					.consume(System.currentTimeMillis())
+					.consume(stunResponseEvent.getNonce())
+					.consume(address.ip())
+					.consume(address.shortPort())
+					.build();
+			System.out.println("Nonce: " + stunResponseEvent.getNonce());
+			context().sendPacket(packet);
+		}, true);
+		addHandler(GameEvent.class, event -> {
+			throw new RuntimeException("Was expecting Bootstrap Request");
+		});
+	}
 
+	@Override
+	public void update() {
 	}
 
 }
