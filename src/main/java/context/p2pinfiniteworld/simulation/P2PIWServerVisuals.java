a8820599fe2cf89bfbf6bc3b6e9a8380714c9da6
Lunkle <bobbob.donny@gmail.com>
2021-12-26 17:18:15 -0500

    Better colours

--- src/main/java/context/p2pinfiniteworld/simulation/P2PIWServerVisuals.java
+++ src/main/java/context/p2pinfiniteworld/simulation/P2PIWServerVisuals.java
@@ -19,6 +19,11 @@ public class P2PIWServerVisuals extends GameVisuals {
 	private static final int REGION_SQUARE_SIZE = GRID_SQUARE_SIZE * 4;
 	private static final int GRID_START_X = 96;
 
+	private static final int NOMAD_COLOUR = rgb(10, 125, 175);
+	private static final int NEARBY_CHUNKS_COLOUR = rgb(104, 166, 68);
+	private static final int CHUNK_BORDER_COLOUR = rgb(111, 115, 122);
+	private static final int REGION_BORDER_COLOUR = rgb(255, 255, 255);
+
 	private GameFont baloo2;
 	private GameFont langar;
 	private TextRenderer textRenderer;
@@ -28,9 +33,6 @@ public class P2PIWServerVisuals extends GameVisuals {
 	private P2PIWServerData data;
 	private LineRenderer lineRenderer;
 	private Texture tinyNomad;
-
-	private int chunkColour = rgb(111, 115, 122);
-	private int regionColour = rgb(255, 255, 255);
 
 	@Override
 	public void init() {
@@ -43,12 +45,11 @@ public class P2PIWServerVisuals extends GameVisuals {
 		rectangleRenderer = new RectangleRenderer(resourcePack().defaultShaderProgram(), resourcePack().rectangleVAO());
 		lineRenderer = resourcePack().getRenderer("line", LineRenderer.class);
 		tinyNomad = resourcePack.getTexture("tiny_nomad");
-		data.nomads().add(new NomadTiny(10, 10));
 	}
 
 	@Override
 	public void render() {
-		background(rgb(47, 49, 54));
+		background(rgb(54, 57, 63));
 		drawGrid();
 		drawNomads();
 	}
@@ -66,24 +67,25 @@ public class P2PIWServerVisuals extends GameVisuals {
 
 	private void drawChunks() {
 		for (int i = 0; i < 60; i++) {
-			lineRenderer.renderPixelCoords(glContext(), rootGui.dimensions(), GRID_START_X + i * GRID_SQUARE_SIZE, 0, GRID_START_X + i * GRID_SQUARE_SIZE, rootGui.dimensions().y, 5, chunkColour);
+			lineRenderer.renderPixelCoords(glContext(), rootGui.dimensions(), GRID_START_X + i * GRID_SQUARE_SIZE, 0, GRID_START_X + i * GRID_SQUARE_SIZE, rootGui.dimensions().y, 5, CHUNK_BORDER_COLOUR);
 		}
 		for (int i = 0; i < 40; i++) {
-			lineRenderer.renderPixelCoords(glContext(), rootGui.dimensions(), GRID_START_X, i * GRID_SQUARE_SIZE, rootGui.dimensions().x, i * GRID_SQUARE_SIZE, 5, chunkColour);
+			lineRenderer.renderPixelCoords(glContext(), rootGui.dimensions(), GRID_START_X, i * GRID_SQUARE_SIZE, rootGui.dimensions().x, i * GRID_SQUARE_SIZE, 5, CHUNK_BORDER_COLOUR);
 		}
 	}
 
 	private void drawRegions() {
 		for (int i = 0; i < 60; i++) {
-			lineRenderer.renderPixelCoords(glContext(), rootGui.dimensions(), GRID_START_X + i * REGION_SQUARE_SIZE, 0, GRID_START_X + i * REGION_SQUARE_SIZE, rootGui.dimensions().y, 5, regionColour);
+			lineRenderer.renderPixelCoords(glContext(), rootGui.dimensions(), GRID_START_X + i * REGION_SQUARE_SIZE, 0, GRID_START_X + i * REGION_SQUARE_SIZE, rootGui.dimensions().y, 5, REGION_BORDER_COLOUR);
 		}
 		for (int i = 0; i < 40; i++) {
-			lineRenderer.renderPixelCoords(glContext(), rootGui.dimensions(), GRID_START_X, i * REGION_SQUARE_SIZE, rootGui.dimensions().x, i * REGION_SQUARE_SIZE, 5, regionColour);
+			lineRenderer.renderPixelCoords(glContext(), rootGui.dimensions(), GRID_START_X, i * REGION_SQUARE_SIZE, rootGui.dimensions().x, i * REGION_SQUARE_SIZE, 5, REGION_BORDER_COLOUR);
 		}
 	}
 
 	private void renderNomad(NomadTiny nomad) {
-		diffuseTextureRenderer.render(glContext(), rootGui, tinyNomad, nomad.x * GRID_SQUARE_SIZE + 2, nomad.y * GRID_SQUARE_SIZE + 2, 28, 28, chunkColour);
+//		diffuseTextureRenderer.render(glContext(), rootGui, tinyNomad, GRID_START_X + nomad.x * GRID_SQUARE_SIZE + 2, nomad.y * GRID_SQUARE_SIZE + 2, 28, 28, );
+		diffuseTextureRenderer.render(glContext(), rootGui, tinyNomad, GRID_START_X + nomad.x * GRID_SQUARE_SIZE + 2, nomad.y * GRID_SQUARE_SIZE + 2, 28, 28, NOMAD_COLOUR);
 	}
 
 }
