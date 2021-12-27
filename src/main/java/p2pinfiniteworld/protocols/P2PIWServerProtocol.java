6a31883c943b00c5b9c9c36c3acd1912d74284ea
Misha Melnyk <as.k.github@melnyk.org>
2021-12-27 18:01:40 -0500

    Protocol - Added Chunk data receive event

--- src/main/java/p2pinfiniteworld/protocols/P2PIWServerProtocol.java
+++ src/main/java/p2pinfiniteworld/protocols/P2PIWServerProtocol.java
@@ -7,21 +7,27 @@ import static context.input.networking.packet.PacketPrimitive.BYTE;
 import context.input.networking.packet.PacketFormat;
 import p2pinfiniteworld.event.P2PIWJoinWorldRequestEvent;
 import p2pinfiniteworld.event.P2PIWNomadMoveNotificationEvent;
+import p2pinfiniteworld.event.P2PIWClickChunkNotificationEvent;
+import p2pinfiniteworld.event.P2PIWChunkDataReceiveEvent;
 import p2pinfiniteworld.event.P2PIWServerGameEvent;
 
 public enum P2PIWServerProtocol {
 
 	/** id, region_x, region_y */
 	MOVE_NOTIFICATION(P2PIWNomadMoveNotificationEvent.class, 100, new PacketFormat().with(INT, INT, INT)),
+	
+	/** chunk_x, chunk_y, age, value*/
+	CHUNK_DATA(P2PIWChunkDataReceiveEvent.class, 102, new PacketFormat().with(INT, INT, INT, INT)),
+	
+	/** id, chunk_x, chunk_y*/
+	CLICK_CHUNK_NOTIFICATION(P2PIWClickChunkNotificationEvent.class, 104, new PacketFormat().with(INT, INT, INT)),
 
 	/** username */
 	JOIN_WORLD_REQUEST(P2PIWJoinWorldRequestEvent.class, 111, new PacketFormat().with(STRING)),
 
 	/** username */
-	JOIN_WORLD_RESPONSE(P2PIWJoinWorldRequestEvent.class, 112, new PacketFormat().with(STRING)),
+	JOIN_WORLD_RESPONSE(P2PIWJoinWorldRequestEvent.class, 112, new PacketFormat().with(STRING));
 	
-	/** id, chunk_x, chunk_y*/
-	CLICK_CHUNK_NOTIFICATION(P2PIWNomadMoveNotificationEvent.class, 105, new PacketFormat().with(INT, INT, INT));
 
 	private short id;
 	private Class<? extends P2PIWServerGameEvent> clazz;
