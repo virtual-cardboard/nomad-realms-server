8988adba913d16070542b58f97dac4d93a0444eb
Misha Melnyk <as.k.github@melnyk.org>
2021-12-27 18:20:54 -0500

    Protocol - Chunk Data Request

---
+++ src/main/java/p2pinfiniteworld/event/P2PIWChunkDataRequestEvent.java
@@ -0,0 +1,50 @@
+package p2pinfiniteworld.event;
+
+import static p2pinfiniteworld.protocols.P2PIWServerProtocol.CHUNK_DATA_REQUEST;
+
+import common.source.NetworkSource;
+import context.input.networking.packet.PacketBuilder;
+import context.input.networking.packet.PacketModel;
+import context.input.networking.packet.PacketReader;
+import p2pinfiniteworld.protocols.P2PIWServerProtocol;
+
+public class P2PIWChunkDataRequestEvent extends P2PIWServerGameEvent {
+
+	private int chunkX, chunkY;
+	private long timestamp;
+
+	public P2PIWChunkDataRequestEvent(NetworkSource source, PacketReader reader) {
+		super(source);
+		this.chunkX = reader.readInt();
+		this.chunkY = reader.readInt();
+		this.timestamp = reader.readLong();
+		reader.close();
+	}
+
+	@Override
+	protected PacketModel toPacketModel(PacketBuilder builder) {
+		return builder
+				.consume(chunkX)
+				.consume(chunkY)
+				.consume(timestamp)
+				.build();
+	}
+
+	@Override
+	protected P2PIWServerProtocol protocol() {
+		return CHUNK_DATA_REQUEST;
+	}
+
+	public int chunkX() {
+		return chunkX;
+	}
+
+	public int chunkY() {
+		return chunkY;
+	}
+	
+	public long timestamp() {
+		return timestamp;
+	}
+
+}
--- src/main/java/p2pinfiniteworld/protocols/P2PIWServerProtocol.java
+++ src/main/java/p2pinfiniteworld/protocols/P2PIWServerProtocol.java
@@ -3,12 +3,14 @@ package p2pinfiniteworld.protocols;
 import static context.input.networking.packet.PacketPrimitive.INT;
 import static context.input.networking.packet.PacketPrimitive.STRING;
 import static context.input.networking.packet.PacketPrimitive.BYTE;
+import static context.input.networking.packet.PacketPrimitive.LONG;
 
 import context.input.networking.packet.PacketFormat;
 import p2pinfiniteworld.event.P2PIWJoinWorldRequestEvent;
 import p2pinfiniteworld.event.P2PIWNomadMoveNotificationEvent;
 import p2pinfiniteworld.event.P2PIWClickChunkNotificationEvent;
 import p2pinfiniteworld.event.P2PIWChunkDataReceiveEvent;
+import p2pinfiniteworld.event.P2PIWChunkDataRequestEvent;
 import p2pinfiniteworld.event.P2PIWServerGameEvent;
 
 public enum P2PIWServerProtocol {
@@ -16,8 +18,11 @@ public enum P2PIWServerProtocol {
 	/** id, region_x, region_y */
 	MOVE_NOTIFICATION(P2PIWNomadMoveNotificationEvent.class, 100, new PacketFormat().with(INT, INT, INT)),
 	
+	/** chunk_x, chunk_y, timestamp*/
+	CHUNK_DATA_REQUEST(P2PIWChunkDataRequestEvent.class, 102, new PacketFormat().with(INT, INT, LONG)),
+	
 	/** chunk_x, chunk_y, age, value*/
-	CHUNK_DATA(P2PIWChunkDataReceiveEvent.class, 102, new PacketFormat().with(INT, INT, INT, INT)),
+	CHUNK_DATA(P2PIWChunkDataReceiveEvent.class, 103, new PacketFormat().with(INT, INT, INT, INT)),
 	
 	/** id, chunk_x, chunk_y*/
 	CLICK_CHUNK_NOTIFICATION(P2PIWClickChunkNotificationEvent.class, 104, new PacketFormat().with(INT, INT, INT)),
