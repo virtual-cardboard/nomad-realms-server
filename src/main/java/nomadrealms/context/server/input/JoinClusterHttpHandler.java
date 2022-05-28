0c3642c87ccbe01ae91f1d8e29bc19b0648a6fba
JaryJay <jay.jrjren@gmail.com>
2022-05-27 22:10:03 -0400

    Update JoinClusterHttpHandler.java

--- src/main/java/nomadrealms/context/server/input/JoinClusterHttpHandler.java
+++ src/main/java/nomadrealms/context/server/input/JoinClusterHttpHandler.java
@@ -1,22 +1,32 @@
 package nomadrealms.context.server.input;
 
+import static java.lang.System.currentTimeMillis;
+import static java.util.stream.Collectors.toList;
+
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import engine.common.networking.packet.address.PacketAddress;
 import event.network.c2s.JoinClusterRequestEvent;
 import event.network.c2s.JoinClusterResponseEvent;
+import event.network.p2p.s2c.JoiningPlayerNetworkEvent;
+import networking.NetworkCluster;
+import networking.PlayerData;
 import nomadrealms.context.server.ServerData;
+import nomadrealms.model.WorldInfo;
 
 public class JoinClusterHttpHandler implements HttpHandler {
 
+	private final ServerData data;
 	private final byte[] bytes = new byte[65536];
 
 	public JoinClusterHttpHandler(ServerData data) {
+		this.data = data;
 	}
 
 	@Override
@@ -24,14 +34,37 @@ public class JoinClusterHttpHandler implements HttpHandler {
 		t.getRemoteAddress();
 		String query = t.getRequestURI().getQuery();
 //		t.getRequestHeaders().forEach((s, list) -> System.out.println(s + ": " + list));
-		String clientWanAddress = t.getRemoteAddress().getAddress().toString();
-		int clientWanPort = t.getRemoteAddress().getPort();
-		System.out.println("Received request from " + clientWanAddress + " port=" + clientWanPort);
+		PacketAddress clientAddress = new PacketAddress(t.getRemoteAddress().getAddress(), t.getRemoteAddress().getPort());
+		System.out.println("Received request from " + clientAddress);
 		int numRead = t.getRequestBody().read(bytes);
 		try {
 			JoinClusterRequestEvent request = new JoinClusterRequestEvent(Arrays.copyOfRange(bytes, 0, numRead));
 			long worldId = request.worldID();
-			System.out.println(clientWanAddress + ":" + clientWanPort + " is trying to join world with Id=" + worldId);
+			System.out.println(clientAddress + " is trying to join world with Id=" + worldId);
+			if (data.getCluster(worldId) == null) {
+				data.addCluster(new NetworkCluster(new WorldInfo(0, "This is a test", 12345)));
+			}
+			NetworkCluster cluster = data.getCluster(worldId);
+			long nonce = new Random().nextLong();
+			cluster.peers().forEach(peer -> {
+				JoiningPlayerNetworkEvent joiningPlayerEvent = new JoiningPlayerNetworkEvent(currentTimeMillis(), nonce, request.lanAddress(), clientAddress);
+				data.context().sendPacket(joiningPlayerEvent.toPacketModel(peer.wanAddress));
+			});
+			List<PacketAddress> peerLanAddresses = cluster.peers().stream().map(p -> p.lanAddress).collect(toList());
+			List<PacketAddress> peerWanAddresses = cluster.peers().stream().map(p -> p.wanAddress).collect(toList());
+			cluster.addPeer(new PlayerData(clientAddress, request.lanAddress(), request.username()));
+			JoinClusterResponseEvent response = new JoinClusterResponseEvent(nonce, peerLanAddresses, peerWanAddresses);
+			byte[] serializedResponse = response.serialize();
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			t.sendResponseHeaders(200, serializedResponse.length);
+			OutputStream os = t.getResponseBody();
+			os.write(serializedResponse);
+			os.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Could not create JoinClusterRequestEvent from the HTTP request body. 400 Bad Request");
@@ -39,20 +72,7 @@ public class JoinClusterHttpHandler implements HttpHandler {
 			OutputStream os = t.getResponseBody();
 			os.write(new byte[] {});
 			os.close();
-			return;
 		}
-		JoinClusterResponseEvent response = new JoinClusterResponseEvent(new Random().nextLong(), new ArrayList<>(), new ArrayList<>());
-		byte[] serializedResponse = response.serialize();
-		t.sendResponseHeaders(200, serializedResponse.length);
-
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		OutputStream os = t.getResponseBody();
-		os.write(serializedResponse);
-		os.close();
 	}
 
 }
