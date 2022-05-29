5240c58010921a11092041f1e87ed7f6ad82d048
JaryJay <jay.jrjren@gmail.com>
2022-05-29 17:33:41 -0400

    Fix JoinClusterSuccessEventHandler

--- src/main/java/nomadrealms/context/server/input/JoinClusterSuccessEventHandler.java
+++ src/main/java/nomadrealms/context/server/input/JoinClusterSuccessEventHandler.java
@@ -1,6 +1,7 @@
 package nomadrealms.context.server.input;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -19,6 +20,9 @@ public class JoinClusterSuccessEventHandler implements HttpHandler {
 	public void handle(HttpExchange t) throws IOException {
 		PacketAddress clientAddress = new PacketAddress(t.getRemoteAddress().getAddress(), t.getRemoteAddress().getPort());
 		System.out.println("Client at " + clientAddress + " has successfully joined cluster.");
+		t.sendResponseHeaders(200, 0);
+		OutputStream os = t.getResponseBody();
+		os.close();
 	}
 
 }
