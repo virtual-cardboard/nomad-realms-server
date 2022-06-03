4e673948d9be8f991c992468ab4a5c5fce8974e5
JaryJay <jay.jrjren@gmail.com>
2022-06-02 22:14:02 -0400

    Update TimeRequestHandler.java

--- src/main/java/nomadrealms/context/server/input/TimeRequestHandler.java
+++ src/main/java/nomadrealms/context/server/input/TimeRequestHandler.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import event.network.c2s.TimeResponseEvent;
 
 public class TimeRequestHandler implements HttpHandler {
 
@@ -16,21 +17,14 @@ public class TimeRequestHandler implements HttpHandler {
 
 	@Override
 	public void handle(HttpExchange t) throws IOException {
-		t.sendResponseHeaders(200, Long.BYTES);
+		System.out.println("Received time request from " + t.getRemoteAddress());
+		long receiveTime = currentTimeMillis();
+		TimeResponseEvent response = new TimeResponseEvent(receiveTime, currentTimeMillis());
+		byte[] bytes = response.serialize();
+		t.sendResponseHeaders(200, bytes.length);
 		OutputStream os = t.getResponseBody();
-		byte[] bytes = new byte[8];
-
-		long time = currentTimeMillis();
-
-		bytes[0] = (byte) ((time >> 56) & 0xFF);
-		bytes[1] = (byte) ((time >> 48) & 0xFF);
-		bytes[2] = (byte) ((time >> 40) & 0xFF);
-		bytes[3] = (byte) ((time >> 32) & 0xFF);
-		bytes[4] = (byte) ((time >> 24) & 0xFF);
-		bytes[5] = (byte) ((time >> 16) & 0xFF);
-		bytes[6] = (byte) ((time >> 8) & 0xFF);
-		bytes[7] = (byte) (time & 0xFF);
 		os.write(bytes);
+		os.close();
 	}
 
 }
