45696807f2c97bd0351ad582ba632c61b76a4efa
JaryJay <jay.jrjren@gmail.com>
2022-06-19 20:24:08 -0400

    Update NomadRealmsDatabase.java

--- src/main/java/nomadrealms/context/server/data/db/NomadRealmsDatabase.java
+++ src/main/java/nomadrealms/context/server/data/db/NomadRealmsDatabase.java
@@ -1,24 +1,43 @@
 package nomadrealms.context.server.data.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.postgresql.ds.PGSimpleDataSource;
 
 public class NomadRealmsDatabase {
 
 	private boolean connected;
 
+	private PGSimpleDataSource dbSource;
+
 	public void connect() {
 		if (connected) {
 			throw new IllegalStateException("Database already connected.");
 		} else {
-			PGSimpleDataSource ds = new PGSimpleDataSource();
-			ds.setUrl("jdbc:postgresql://free-tier11.gcp-us-east1.cockroachlabs.cloud:26257/defaultdb?options=--cluster%3Dnomad-realms-cluster-1145&sslmode=verify-full");
-			ds.setUser("virtualcardboard");
-			ds.setPassword("RSlnFuOi6hXgofBjvNZ95A");
+			dbSource = new PGSimpleDataSource();
+			dbSource.setUrl("jdbc:postgresql://free-tier11.gcp-us-east1.cockroachlabs.cloud:26257/defaultdb?options=--cluster%3Dnomad-realms-cluster-1145&sslmode=verify-full");
+			dbSource.setUser("virtualcardboard");
+			dbSource.setPassword("RSlnFuOi6hXgofBjvNZ95A");
 			connected = true;
 		}
 	}
 
 	public void testing() {
+		String sql = "";
+		try (Connection connection = dbSource.getConnection()) {
+			try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+				if (pstmt.execute()) {
+					ResultSet resultSet = pstmt.getResultSet();
+				} else {
+					System.out.println("Aaaahhhh!!!!");
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
